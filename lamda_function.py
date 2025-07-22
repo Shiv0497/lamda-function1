04b7aeebd5d3f1ef3243d330d2adc56a22dbf43b
Shiv0497 <shvmhdb@gmail.com>
2025-07-22 15:53:37 +0530

    test

--- lamda_function.py
+++ lamda_function.py
@@ -31,5 +31,8 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps(f"Error: {str(e)}")
         }
-        
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Error: {str(e)}")
+        }
         
