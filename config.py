cd18158c462fdec131cb769ad593465e4c8cc38f
Shiv0497 <shvmhdb@gmail.com>
2025-07-22 17:43:36 +0530

    updated code test

---
+++ config.py
@@ -0,0 +1,11 @@
+LOGIN_TYPE = 2
+
+REDIRECTION = 3       
+
+MOODLE_PROTOCOL = "https://"
+MOODLE_URL = "awslmsdev2.quiklrn.com/lmsdev2"
+MOODLE_LOGIN_URL = "awslmsdev2.quiklrn.com"
+MOODLE_TOKEN = "58cd3c33381335b244e8c6ca61d59127"       # Enroll users token
+MOODLE_LOGIN_TOKEN = "9e6a379647b679041f708ece4cbac585" # Login-specific token
+
+WEBSITE_URL = "https://awslmsdev2.quiklrn.com/lmsdev2"
